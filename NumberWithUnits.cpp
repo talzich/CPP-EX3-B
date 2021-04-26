ca6393a3b7f239d56b499bfd640d6bd171336c05
Tal <talzhtc@gmail.com>
2021-04-26 15:30:32 +0300

    Tested file read

--- NumberWithUnits.cpp
+++ NumberWithUnits.cpp
@@ -4,102 +4,129 @@
 // (https://github.com/talzich/CPP-EX3-A/blob/master/README.md)
 
 #include <iostream>
+#include <fstream>
 
 #include "NumberWithUnits.hpp"
 
-using namespace ariel;
 using namespace std;
 
-NumberWithUnits::NumberWithUnits(double num, string unit){
-    
+namespace ariel{
+
+    const string UNIT_FILE_PATH = "units.txt";
+
+    NumberWithUnits::NumberWithUnits(double num, string unit){
+        
+    }
+
+    void NumberWithUnits::read_units(std::ifstream &u_file){
+        
+        // Opening file
+        u_file.open(UNIT_FILE_PATH);
+        if(!u_file){
+            cerr << "Unable to open file " << UNIT_FILE_PATH <<endl;
+            exit(1);
+        }
+
+        char c = ' ';
+        while(u_file >> c){
+            cout << c << endl;
+        }
+
+        u_file.close();
+
+    }
+
+    //-----------------------------
+    // Arithmetic Operators
+    //-----------------------------
+
+    NumberWithUnits NumberWithUnits::operator-(){
+        return *this;
+    }
+
+    NumberWithUnits NumberWithUnits::operator-(NumberWithUnits num){
+        return *this;
+    }
+    NumberWithUnits NumberWithUnits::operator-=(NumberWithUnits num){
+        return *this;
+    }
+
+    NumberWithUnits NumberWithUnits::operator+(){
+        return *this;
+    }
+    NumberWithUnits NumberWithUnits::operator+(NumberWithUnits num){
+        return *this;
+    }
+    NumberWithUnits NumberWithUnits::operator+=(NumberWithUnits num){
+        return *this;
+    }
+
+    //-----------------------------
+    // Comparison Operators
+    //-----------------------------
+
+    bool operator!=(NumberWithUnits num1, NumberWithUnits num2){
+        return false;
+    }
+    bool operator==(NumberWithUnits num1, NumberWithUnits num2){
+        return false;
+    }
+
+    bool operator<=(NumberWithUnits num1, NumberWithUnits num2){
+        return false;
+    }
+    bool operator>=(NumberWithUnits num1, NumberWithUnits num2){
+        return false;
+    }
+
+    bool operator<(NumberWithUnits num1, NumberWithUnits num2){
+        return false;
+    }
+    bool operator>(NumberWithUnits num1, NumberWithUnits num2){
+        return false;
+    }
+
+    //-----------------------------
+    // Increment Decrement Operators
+    //-----------------------------
+
+    NumberWithUnits NumberWithUnits::operator--(){
+        return *this;
+    }
+    NumberWithUnits NumberWithUnits::operator--(int dummy){
+        return *this;
+    }
+
+    NumberWithUnits NumberWithUnits::operator++(){
+        return *this;
+    }
+    NumberWithUnits NumberWithUnits::operator++(int dummy){
+        return *this;
+    }
+
+    //-----------------------------
+    // Friend Operators
+    //-----------------------------
+
+    NumberWithUnits operator*(double d, NumberWithUnits num){
+        return num;
+    }
+
+    //-----------------------------
+    // I/O Operators
+    //-----------------------------
+    ostream& operator<<(ostream &out, NumberWithUnits num){
+        return out;
+    }
+    istream& operator>>(istream &in, NumberWithUnits num){
+        return in;
+    }
+
 }
 
-void NumberWithUnits::read_units(std::ifstream &u_fle){
-
-}
-
-//-----------------------------
-// Arithmetic Operators
-//-----------------------------
-
-NumberWithUnits NumberWithUnits::operator-(){
-    return *this;
-}
-
-NumberWithUnits NumberWithUnits::operator-(NumberWithUnits num){
-    return *this;
-}
-NumberWithUnits NumberWithUnits::operator-=(NumberWithUnits num){
-    return *this;
-}
-
-NumberWithUnits NumberWithUnits::operator+(){
-    return *this;
-}
-NumberWithUnits NumberWithUnits::operator+(NumberWithUnits num){
-    return *this;
-}
-NumberWithUnits NumberWithUnits::operator+=(NumberWithUnits num){
-    return *this;
-}
-
-//-----------------------------
-// Comparison Operators
-//-----------------------------
-
-bool ariel::operator!=(NumberWithUnits num1, NumberWithUnits num2){
-    return false;
-}
-bool ariel::operator==(NumberWithUnits num1, NumberWithUnits num2){
-    return false;
-}
-
-bool ariel::operator<=(NumberWithUnits num1, NumberWithUnits num2){
-    return false;
-}
-bool ariel::operator>=(NumberWithUnits num1, NumberWithUnits num2){
-    return false;
-}
-
-bool ariel::operator<(NumberWithUnits num1, NumberWithUnits num2){
-    return false;
-}
-bool ariel::operator>(NumberWithUnits num1, NumberWithUnits num2){
-    return false;
-}
-
-//-----------------------------
-// Increment Decrement Operators
-//-----------------------------
-
-NumberWithUnits NumberWithUnits::operator--(){
-    return *this;
-}
-NumberWithUnits NumberWithUnits::operator--(int dummy){
-    return *this;
-}
-
-NumberWithUnits NumberWithUnits::operator++(){
-    return *this;
-}
-NumberWithUnits NumberWithUnits::operator++(int dummy){
-    return *this;
-}
-
-//-----------------------------
-// Friend Operators
-//-----------------------------
-
-NumberWithUnits ariel::operator*(double d, NumberWithUnits num){
-    return num;
-}
-
-//-----------------------------
-// I/O Operators
-//-----------------------------
-ostream& ariel::operator<<(ostream &out, NumberWithUnits num){
-    return out;
-}
-istream& ariel::operator>>(istream &in, NumberWithUnits num){
-    return in;
+int main(void){
+    ariel::NumberWithUnits n{1, "km"};
+    ifstream u_file;
+    n.read_units(u_file);
+    return 0;
 }
