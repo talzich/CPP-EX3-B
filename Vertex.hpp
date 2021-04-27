e1a33e3a8ff9f7bc3930daa6f78e6876f15f07d3
Tal <talzhtc@gmail.com>
2021-04-27 10:52:54 +0300

    Added methods and members

--- Vertex.hpp
+++ Vertex.hpp
@@ -2,9 +2,23 @@
 // AUTHOR: Tal Zichlinsky
 // Header file for class Vertex. This class will be implementing the observer design pattern
 
+#include <map>
+
 namespace zich{
     
     class Vertex{
+        
+        string key;
 
+        public:
+
+            Vertex();
+            Vertex(string key);
+            ~Vertex();
+
+            string get_key();
+
+            int add_nei(string key, double weight);
+            int remove_nei(string key);
     };
 }
