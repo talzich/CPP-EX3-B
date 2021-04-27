090f57329ea2c6736e0ccf3b6860101696746115
Tal <talzhtc@gmail.com>
2021-04-27 11:03:35 +0300

    Added qualifications

--- Vertex.hpp
+++ Vertex.hpp
@@ -2,23 +2,18 @@
 // AUTHOR: Tal Zichlinsky
 // Header file for class Vertex
 
-#include <map>
-
 namespace zich{
     
     class Vertex{
-        
-        string key;
 
         public:
 
-            Vertex();
-            Vertex(string key);
+            Vertex(std::string key);
             ~Vertex();
 
-            string get_key();
+            std::string get_key();
 
-            int add_nei(string key, double weight);
-            int remove_nei(string key);
+            int add_nei(std::string key, double weight);
+            int remove_nei(std::string key);
     };
 }
