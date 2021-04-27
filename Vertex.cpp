4a45c30fd71187c0fcc59203772ffbc04cd137f6
Tal <talzhtc@gmail.com>
2021-04-27 11:56:36 +0300

    Started implementing observer

--- Vertex.cpp
+++ Vertex.cpp
@@ -6,6 +6,7 @@
 
 #include <map>
 #include <string>
+#include <vector>
 
 #include "Vertex.hpp"
 
@@ -15,13 +16,40 @@ namespace zich{
 
     class Vertex{
 
-        // Maps to keep track of neighbors on ingoing or outgoing edges. The ins map will double as an observers(subscribers) list
-        map<string, double> outs;
-        map<string, double> ins;
+        // Maps to keep track of neighbors on ingoing or outgoing edges
+        map<string, double> outs{};
+        map<string, double> ins{};
 
+        vector<Vertex> observers;
         string key;
 
+        void update(Vertex nei, double weight){
+            for(Vertex v : observers){
+                v.add_nei(nei, weight);
+            }
+        }
+
         public:
+            Vertex(string new_key){
+                key = new_key;
+            }
+
+            ~Vertex(){
+            }
+
+            string get_key(){
+                return key;
+            }
+
+            int add_nei(Vertex nei, double weight){
+                outs[nei.get_key()] = weight;
+                ins[nei.get_key()] = 1/weight;
+                update(nei, weight);
+                observers.push_back(nei);
+            }
             
+            
+            // int add_nei_in();
+            int remove_nei(string key);
     };
 }
--- Vertex.hpp
+++ Vertex.hpp
@@ -13,7 +13,8 @@ namespace zich{
 
             std::string get_key();
 
-            int add_nei(std::string key, double weight);
+            int add_nei(Vertex nei, double weight);
+            // int add_nei_in();
             int remove_nei(std::string key);
     };
 }
